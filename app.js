a8d3ba214fbaac8de29d0835010f005cf08fc64a
JoyAssis <joyceassis96@gmail.com>
2025-03-03 23:01:24 -0300

    funcionalidade add: adicionar amigo com atualização de lista

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    if (nome !== "") {
+        amigos.push(nome);
+        input.value = "";
+        atualizarListaAmigos();
+    }
+}
+
+function atualizarListaAmigos() {
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    amigos.forEach(nome => {
+        const li = document.createElement("li");
+        li.textContent = nome;
+        listaAmigos.appendChild(li);
+    });
+}
+
